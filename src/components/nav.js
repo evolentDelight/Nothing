9d332aeb8566a856329564e5eed515811c519b84
evolentDelight <evolentDelight@outlook.com>
2020-07-22 15:38:37 -0700

    nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React from "react"
+import "./nav.css"
 //import css
 
-export default function nav(){
-
-  
-
-  return
+export default function nav() {
+  return (
+    <div className="container">
+      <nav className="nav-flexbox">
+        <div className="company">
+          <a href="">
+            <strong>Nobody</strong>
+          </a>
+        </div>
+        <div className="features">
+          <a href="">a</a>
+          <a href="">b</a>
+          <a href="">c</a>
+        </div>
+      </nav>
+    </div>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./index.css"
+import Nav from "../components/nav.js"
 
 export default function App() {
-  return <h1>Test</h1>
+  return [<Nav />, <p>hi</p>]
 }
