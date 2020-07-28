6329923cf4252773f69ff9e62632b152da780600
evolentDelight <evolentDelight@outlook.com>
2020-07-28 07:54:19 -0700

    Better code with same functionality

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -4,22 +4,20 @@ import "./introduction.css"
 
 export default function Introduction() {
   const [fadeIn, setFadeIn] = useState(false)
-  const [initial, setInitial] = useState(false)
 
   useEffect(() => {
     setFadeIn(true)
-    setInitial(true)
   }, [])
 
   return (
     <div className="introduction-container textStyle">
       <CSSTransition in={fadeIn} classNames="fade" timeout={3500}>
         <div>
-          <p className={`${initial ? "reveal" : "hide"}`}>
+          <p>
             Existing before mankind,
           </p>
-          <p className={`${initial ? "reveal" : "hide"}`}>Even before the</p>
-          <p className={`${initial ? "reveal" : "hide"}`}>Big Bang...</p>
+          <p>Even before the</p>
+          <p>Big Bang...</p>
         </div>
       </CSSTransition>
     </div>
