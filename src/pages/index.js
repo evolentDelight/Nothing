20cd0f5ed4adf3fb966fed67c17c9b473991f3af
evolentDelight <evolentDelight@outlook.com>
2020-07-28 12:54:30 -0700

    Add code to test other code

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from "react"
 import "./index.css"
 import Nav from "../components/nav.js"
 import Introduction from "../components/introduction.js"
+import MainContent from "../components/MainContent.js"
 
 export default function App() {
-  return [<Nav />, <Introduction />]
+  return [<Nav />, <Introduction />, <MainContent />, <p id="pad"></p>]
 }
