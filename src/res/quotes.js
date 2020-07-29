3bcc1030b504e605c10bf76e5e28308349281389
evolentDelight <evolentDelight@outlook.com>
2020-07-29 14:35:01 -0700

    Quotes finished

---
+++ src/res/quotes.js
@@ -0,0 +1,31 @@
+export default function quotes(){
+  return [
+    {
+      name: "Leo Tolstoy",
+      quote:
+        "“We can know only that we know nothing. And that is the highest degree of human wisdom.”",
+    },
+    {
+      name: "Plato",
+      quote:
+        "“I am the wisest man alive, for I know one thing, and that is that I know nothing.”",
+    },
+    {
+      name: "Oscar Wilde",
+      quote:
+        "“I love to talk about nothing. It's the only thing I know anything about.”",
+    },
+    {
+      name: "Ayn Rand",
+      quote: "“If you write a line of zeroes, it´s still nothing.”",
+    },
+    {
+      name: "A.A. Milne",
+      quote: "“doing nothing often leads to the very best of something”",
+    },
+    {
+      name: "A.A. Milne, Winnie-the-Pooh",
+      quote: "“People say nothing is impossible, but I do nothing every day.”",
+    },
+  ];
+}
