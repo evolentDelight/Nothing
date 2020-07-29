7a73d95e50200802207a6be5319fa3de7b6604ba
evolentDelight <evolentDelight@outlook.com>
2020-07-29 08:49:08 -0700

    Add Component-Topic

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import "./index.css"
 import Nav from "../components/nav.js"
 import Introduction from "../components/introduction.js"
 import MainContent from "../components/MainContent.js"
+import Topic from "../components/Topic.js"
 
 export default function App() {
-  return [<Nav />, <Introduction />, <MainContent />, <p id="pad"></p>]
+  return [<Nav />, <Introduction />, <MainContent />, <Topic />, <p id="pad"></p>]
 }
