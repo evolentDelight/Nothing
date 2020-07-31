ba0753229f3487eb4cfdd82afe092f99069a01fd
evolentDelight <evolentDelight@outlook.com>
2020-07-31 09:07:04 -0700

    Security edits

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,16 +6,16 @@ export default function Nav() {
     <div className="container-nav">
       <nav className="nav-flexbox">
         <div className="company">
-        <a href="https://github.com/evolentDelight" target="_blank">
+        <a href="https://github.com/evolentDelight" target="_blank" rel="noreferrer">
             <strong><em>Nobody</em></strong>
           </a>
         </div>
         <div className="features">
-          <a href="https://www.google.com/" target="_blank">Explore</a>
+          <a href="https://www.google.com/" target="_blank" rel="noreferrer">Explore</a>
           <span className="nav-features-space"> </span>
-          <a href="/">Support</a>
+          <a href="/" rel="noreferrer">Support</a>
           <span className="nav-features-space"> </span>
-          <a href="/">Author</a>
+          <a href="https://github.com/evolentDelight" target="_blank" rel="noreferrer">Author</a>
           <span className="nav-features-space"> </span>
         </div>
       </nav>
